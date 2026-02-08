2c91ef63d90cd8f0692234a22c7018cc447b5c9a
Namefix <41016273+Namefix@users.noreply.github.com>
2026-02-09 02:35:15 +0300

    Added fabric datagen
    - Added fabric data generation
    - Added drops for tobacco crop and wild tobacco

---
+++ fabric/src/main/java/com/namefix/fabric/datagen/DeadeyeBlockLootProvider.java
@@ -0,0 +1,27 @@
+package com.namefix.fabric.datagen;
+
+import com.namefix.block.TobaccoCropBlock;
+import com.namefix.registry.BlockRegistry;
+import com.namefix.registry.ItemRegistry;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+import net.minecraft.advancements.critereon.StatePropertiesPredicate;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.world.level.storage.loot.predicates.LootItemBlockStatePropertyCondition;
+
+import java.util.concurrent.CompletableFuture;
+
+public class DeadeyeBlockLootProvider extends FabricBlockLootTableProvider {
+	protected DeadeyeBlockLootProvider(FabricDataOutput dataOutput, CompletableFuture<HolderLookup.Provider> registryLookup) {
+		super(dataOutput, registryLookup);
+	}
+
+	@Override
+	public void generate() {
+		LootItemBlockStatePropertyCondition.Builder builder = LootItemBlockStatePropertyCondition.hasBlockStateProperties(BlockRegistry.TOBACCO_CROP.get())
+						.setProperties(StatePropertiesPredicate.Builder.properties().hasProperty(TobaccoCropBlock.AGE, TobaccoCropBlock.MAX_AGE));
+		add(BlockRegistry.TOBACCO_CROP.get(), createCropDrops(BlockRegistry.TOBACCO_CROP.get(), ItemRegistry.TOBACCO.get(), ItemRegistry.TOBACCO_SEEDS.get(), builder));
+
+		dropOther(BlockRegistry.WILD_TOBACCO.get(), ItemRegistry.TOBACCO.get());
+	}
+}
